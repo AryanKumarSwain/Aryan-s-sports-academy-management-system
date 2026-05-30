78c47d743f649f97faf251b147bddb161f1144f1
Aryan Kumar Swain <aryannn.ks@gmail.com>
2026-05-30 11:54:17 +0530

    up

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-aeb6ecaf'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.f4djivcdgv"
+    "revision": "0.6m14r8hebpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
